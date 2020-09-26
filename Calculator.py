04be6f7da36502beeb54975276118d5d87317806
joey-development <71944361+joey-development@users.noreply.github.com>
2020-09-26 20:32:11 +0100

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+
+import time
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("invalid operator")
+    time.sleep(5)
+
+
+time.sleep(5)
